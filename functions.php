05935ba2e1568ea624f726a702adec4991e20c01
Marco Valeri <marcovaleri@hotmail.it>
2022-07-09 22:41:30 +0100

    Set up assets files

--- functions.php
+++ functions.php
@@ -2,7 +2,8 @@
 
 // Load main CSS file
 function viaggin_enqueque_style() {
-    wp_enqueue_style('main-css', get_stylesheet_directory_uri() . '/style.css', [], time(), 'all');
+    wp_enqueue_style('style-css', get_stylesheet_directory_uri() . '/style.css', [], time(), 'all');
+    wp_enqueue_style('main-css', get_stylesheet_directory_uri() . '/assets/css/main.css', ['style-css'], time(), 'all');
 }
 
 add_action('wp_enqueue_scripts', 'viaggin_enqueque_style');
