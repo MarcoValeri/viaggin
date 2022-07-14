c6e2c8ceeb98edec4c03f8b5a91dbcd5f945bf5f
Marco Valeri <marcovaleri@hotmail.it>
2022-07-14 22:53:07 +0100

    Create categories components

---
+++ home.php
@@ -0,0 +1,21 @@
+<!-- Include header.php -->
+<?php get_header(); ?>
+
+        <main class="home__main">
+            <div class="home__grid-categories">
+                <div class="home__container home__container-viaggi">
+                    Viaggi
+                </div>
+                <div class="home__container home__container-eventi">
+                    Eventi
+                </div>
+                <div class="home__container home__container-documenti">
+                    Documenti
+                </div>
+                <div class="home__container home__container-estero">
+                    Vivere all'estero
+                </div>
+            </div>
+        </main>
+    </body>
+</html>
