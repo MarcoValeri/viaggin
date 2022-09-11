4eb8fe8dd8ba3148519e759f5a945e729b7b2911
Marco Valeri <marcovaleri@hotmail.it>
2022-09-11 19:53:16 +0100

    Add style for comment form

--- single.php
+++ single.php
@@ -73,24 +73,25 @@ if (have_posts()) {
         <section id="article-container-comments" class="article__container-comments-form">
             <h3 class="article__comments-sub-title h3">Lascia un commento</h3>
             <?php
+            $userEmailLogged = wp_get_current_user()->user_email;
             $argsComment = [
                 'fields' => [
-                    'author'    => '<div class="article__form-comments-name article__input"><input id="author" name="author" placeholder="Nome *" /></div>',
-                    'email'     => '<div class="article__form-comments-email article__input"><input id="email" name="email" placeholder="Email *" /></div>',
-                    'cookies'   => '<div class="article__form-comments-privacy article__input"><input type="checkbox" id="Privacy" name="Privacy" required="required" value="1"><label class="contact__privacy-label required" for="comment_form_privacy">Accetto la privacy policy</label></div><div class="article__form-comments-submit article__input"><p class="body-3">Per maggiori informazioni consulta la <a href="#">Privacy Policy di ViaggIn</a></p></div>'
+                    'author'    => '<div class="article__form-comments-name article__input"><input type="text" id="author" name="author" placeholder="Nome *" /></div>',
+                    'email'     => '<div class="article__form-comments-email article__input"><input type="email" id="email" name="email" placeholder="Email *" /></div>',
+                    'cookies'   => '<div class="article__form-comments-privacy article__input"><input type="checkbox" id="Privacy" name="Privacy" required="required" value="1"><label class="contact__privacy-label body-3 required" for="Privacy">Accetto la privacy policy</label></div><div class="article__form-comments-submit article__input"><p class="body-3">Per maggiori informazioni consulta la <a href="#">Privacy Policy di ViaggIn</a></p></div>'
                 ],
-                'label_submit'              => __('Invia'),
-                'title_reply'               => __('La tua email non verrà pubblicata'),
+                'submit_button'             => '<div class="article__form-comments-button"><input class="button button--black" type="submit" name="submit" value="Invia" /></div>',
+                'title_reply'               => __('*** La tua email non verrà pubblicata ***'),
                 'title_reply_to'            => __('Rispondi'),
                 'comment_field'             => '<div class="article__form-comments-comment article__input"><textarea id="comment" name="comment" placeholder="Commento *"></textarea></div>',
-                'comment_notes_before'      => __('Hello PHP'),
+                'logged_in_as'              => '<div class="article__form-comments-user article__input"><p class="body-2">Sei registrato come ' . $userEmailLogged . '</p></div>',
+                'comment_notes_before'      => __(''),
                 'comment_notes_after'       => '',
                 'id_submit'                 => __('comment-submit'),
+                'class_form'                => 'article__form-comments-grid'
             ];
             ?>
-            <div class="article__form-comments-grid">
-                <?php comment_form($argsComment); ?>
-            </div>
+            <?php comment_form($argsComment); ?>
         </section>
 <?php
     }
