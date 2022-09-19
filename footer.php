9f8d75215db11449d72ba0158885269f294f34d6
Marco Valeri <marcovaleri@hotmail.it>
2022-09-19 16:16:44 +0100

    Add date on the footer

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
         </div> <!-- Open on hedaer.php with class "content" -->
         <footer class="footer">
             <div class="footer__container-content">
-                <p class="body-3">Made with <span class="footer__heart">&hearts;</span> in London by Marco Valeri - &copy; 1984 - All rights reserved</p>
+                <p class="body-3">Made with <span class="footer__heart">&hearts;</span> in London by Marco Valeri - &copy; <?= date('Y'); ?> - All rights reserved</p>
                 <p class="body-3"><a class="link-no-style link-no-style--white" href="/privacy/">Privacy Policy</a> - <a class="link-no-style link-no-style--white" href="/privacy/">Cookie Policy</a></p>
             </div>
         </footer>
