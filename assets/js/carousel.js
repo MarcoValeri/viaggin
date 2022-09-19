6988bdcbc842d7e05038f4f04fabf8248d2005b8
Marco Valeri <marcovaleri@hotmail.it>
2022-09-19 10:09:53 +0100

    Add carousel gallery

---
+++ assets/js/carousel.js
@@ -0,0 +1,77 @@
+window.onload = () => {
+
+    // DOM variables
+    const carouselLeftBtn = document.getElementById('carousel-left-btn');
+    const carouselRightBtn = document.getElementById('carousel-right-btn');
+    const carouselSlides = document.querySelectorAll('#carousel-slide');
+
+    // Save the number of the carousel into a variable
+    const carouselSlidesNumber = carouselSlides.length;
+
+    // Save number of the slide to display into a variable
+    let slideNumber = 0;
+
+    // Show only the first slide then document load
+    carouselSlides.forEach((element, item) => {
+        
+        if (item > 0) {
+            element.style.display = 'none';
+        }
+
+    })
+
+    // Click right button event
+    carouselRightBtn.addEventListener('click', () => {
+            slideNumber++;
+
+            carouselSlides.forEach((element, item) => {
+                
+                if (item === slideNumber) {
+                    element.style.display = 'block';
+                } else {
+                    element.style.display = 'none';
+                }
+
+                if ((slideNumber + 1) === carouselSlidesNumber) {
+                    carouselRightBtn.style.display = 'none';
+                } else {
+                    carouselRightBtn.style.display = 'block';
+                }
+
+                // Show the left btn if the slideNum is not equal to 0
+                if (slideNumber !== 0) {
+                    carouselLeftBtn.style.display = 'block';
+                }
+
+            })
+
+    })
+
+    // Click left button event
+    carouselLeftBtn.addEventListener('click', () => {
+        slideNumber--;
+
+        carouselSlides.forEach((element, item) => {
+            
+            if (item === slideNumber) {
+                element.style.display = 'block';
+            } else {
+                element.style.display = 'none';
+            }
+
+            if (slideNumber === 0) {
+                carouselLeftBtn.style.display = 'none';
+            } else {
+                carouselLeftBtn.style.display = 'block';
+            }
+
+            // Show right button if it is not last image
+            if ((slideNumber + 1) !== carouselSlidesNumber) {
+                carouselRightBtn.style.display = 'block';
+            }
+
+        })
+
+    })
+
+}
--- functions.php
+++ functions.php
@@ -8,6 +8,8 @@ function viaggin_enqueue_script() {
 
     wp_enqueue_style('style-css', get_stylesheet_directory_uri() . '/style.css', [], time(), 'all');
     wp_enqueue_style('main-css', get_stylesheet_directory_uri() . '/assets/css/main.css', ['style-css'], time(), 'all');
+
+    wp_enqueue_script('carousel', get_stylesheet_directory_uri() . '/assets/js/carousel.js', [], 1, true);
 }
 
 add_action('wp_enqueue_scripts', 'viaggin_enqueue_script');
