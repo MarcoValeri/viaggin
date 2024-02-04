dc7e47440456c63439c9dc710fae8987e1584da3
MarcoValeri <marcovaleri@hotmail.it>
2024-02-04 11:50:42 +0000

    Add Cookie Bot

--- footer.php
+++ footer.php
@@ -9,11 +9,6 @@
                 <p class="body-3"><a class="link-no-style link-no-style--white" href="/privacy/">Privacy Policy</a> - <a class="link-no-style link-no-style--white" href="/privacy/">Cookie Policy</a></p>
             </div>
         </footer>
-        <!-- Google AdSense Privacy Policy AMP -->
-        <amp-consent id="googlefc" layout="nodisplay" type="googlefc"><script type="application/json">{"clientConfig":{"publisherIdentifier":"pub-9306947071363993"}}</script></amp-consent>
-        
-        <!-- Iubenda Privacy and Cookie Policy -->
-        <a href="https://www.iubenda.com/privacy-policy/36030468" class="iubenda-white no-brand iubenda-embed" title="Privacy Policy ">Privacy Policy</a><script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script>
         <?php wp_footer(); ?>
     </body>
 </html>
